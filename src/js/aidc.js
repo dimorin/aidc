4cd7e4f7203781eabfbd81e5a507bfd0d69e00ae
dimorin <dimorinpk@gmail.com>
2024-08-10 11:36:35 +0900

    Add files via upload

--- src/js/aidc.js
+++ src/js/aidc.js
@@ -2,20 +2,22 @@ import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js"
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
 /* import * as dat from '../../node_modules/lil-gui/dist/lil-gui.esm.js' */
 import GUI from 'lil-gui';
 
 let $canvas, scene, camera, renderer, orbitControls;
-let offsetX = 0;   
-let offsetZ = 0;
+let models = [];
+let old_camera_position;
+const raycaster = new THREE.Raycaster(); // create once
+const mouse = new THREE.Vector2();  // create once
 const $btn_init_camera = document.querySelector('.btn_init_camera');
 const clock = new THREE.Clock();
 
 if(WebGL.isWebGLAvailable()){    
     //console.log(THREE);
     init();    // 기본 환경 세팅 : scene, camera, renderer, orbitControls, light    
-    loadMap();
-    set_raycast();
+    loadMap();    
     draw();
     eventListener();
 }else{
@@ -87,65 +89,162 @@ function init(){
 }
 
 async function loadMap(){
-    console.log("0");
-    const gltfLoader = new GLTFLoader();
-    const promises = [
-        gltfLoader.loadAsync("./src/models/office_edgetoface.glb"),
-        gltfLoader.loadAsync("./src/models/technology.glb")
-    ];      
-    console.log("1");
-    const [...model] = await Promise.all(promises);
     
-    // model이 모두 로드 된 후 할 일
-    console.log(model[1]);  
-    let models_scene = model[1].scene.children;
-    models_scene.forEach(function(model, index){
-        //console.log(index, model.name);
-        scene.add(model);        
+    const gltfLoader = new GLTFLoader();
+    const promises = [        
+        gltfLoader.loadAsync("./src/models/server1.glb"),
+        gltfLoader.loadAsync("./src/models/server3.glb"),
+        gltfLoader.loadAsync("./src/models/server4.glb"),
+        gltfLoader.loadAsync("./src/models/tower.glb"),
+        gltfLoader.loadAsync("./src/models/cloud.glb")
+    ];      
+    
+    const [...load_model] = await Promise.all(promises);
+    
+    // model이 모두 로드 된 후 할 일    
+    load_model.forEach(function(model, index){
+        console.log(model.scene.children[0].name, model.scene.children[0].position);
+        models.push(model.scene.children[0]);
+        scene.add(model.scene.children[0]);        
     });
-    let models_scenes = model[1].scenes[0].children;
-    models_scenes.forEach(function(model, index){
-        //console.log(index, model.name);
-        scene.add(model);        
-    });    
+    // floor
+    const floor = new THREE.Mesh(new THREE.CircleGeometry(100), new THREE.MeshPhongMaterial({color:'darkblue', side:THREE.DoubleSide}));
+    floor.name = 'floor';    
+    floor.rotation.x = THREE.MathUtils.degToRad(90);
+    scene.add(floor);
 }
 
-function set_raycast(){
-    // RAYCASTING
-    const raycaster = new THREE.Raycaster(); // create once
-    const mouse = new THREE.Vector2();  // create once
-
-    function intersect(mouse) {
-        raycaster.setFromCamera(mouse, camera);
-        return raycaster.intersectObjects(scene.children);
+// 카메라 이동 함수
+function moveCameraToMesh(mesh) {    
+    let targetPosition;
+    console.log(mesh.parent.type);
+    old_camera_position = {...camera.position};
+    
+    if(mesh.parent.type === 'Group'){
+        if(mesh.parent.position.x < 0){
+            targetPosition = new THREE.Vector3().copy(mesh.parent.position).add(new THREE.Vector3(-25, 15, 50));
+        }else{
+            targetPosition = new THREE.Vector3().copy(mesh.parent.position).add(new THREE.Vector3(25, 15, 50));
+        }
+        
+    }else{
+        if(mesh.position.x < 0){
+            targetPosition = new THREE.Vector3().copy(mesh.position).add(new THREE.Vector3(-25, 15, 50));
+        }else{
+            targetPosition = new THREE.Vector3().copy(mesh.position).add(new THREE.Vector3(25, 15, 50));
+        }
+        
     }
 
-    window.addEventListener('click', event => {
-        // THREE RAYCASTER
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    
-        const intersects = intersect(mouse);
-        
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-            const point = intersect.point;
-            
-            // Convert 3D point to 2D screen position
-            const screenPosition = point.clone().project(camera);
-            const x = (screenPosition.x * 0.5 + 0.5) * window.innerWidth;
-            const y = (screenPosition.y * -0.5 + 0.5) * window.innerHeight;
-
-            // Create and position the div
-            const infoDiv = document.createElement('div');
-            infoDiv.className = 'info-div';
-            infoDiv.style.left = `${x}px`;
-            infoDiv.style.top = `${y}px`;
-            infoDiv.innerHTML = `${intersect.object.name} Clicked at<br>x: ${point.x.toFixed(2)}<br>y: ${point.y.toFixed(2)}<br>z: ${point.z.toFixed(2)}`;
-            document.body.appendChild(infoDiv);
-        }
+    /* gsap.to(camera.position, {
+        duration: 1.5,
+        x: 20,
+        y: 40,
+        z: 40,
+        ease: "power2.inOut"
+    }); */
+    gsap.to(camera.position, {
+        duration: 1.5,
+        x: targetPosition.x,
+        y: targetPosition.y,
+        z: targetPosition.z,
+        ease: "power2.inOut"
     });
+   
 
+    camera.lookAt(mesh.position);
+    
+}
+function intersect(mouse) {
+    raycaster.setFromCamera(mouse, camera);
+    return raycaster.intersectObjects(scene.children);
+}
+function windowClickHandler(event){
+    if(event.target.className === 'btn_init_camera'){
+        return;
+    }
+    
+    // THREE RAYCASTER
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    const intersects = intersect(mouse);
+    
+    if (intersects.length > 0) {
+        const intersect = intersects[0];
+        const point = intersect.point;
+        let clickedMesh = intersects[0].object;
+        let clickedName = intersects[0].object.name;
+        
+        if(intersect.object.parent.type === 'Group'){
+            clickedMesh = intersects[0].object.parent;
+            clickedName = intersects[0].object.parent.name;
+        }
+        
+        console.log(models);
+
+        models.forEach(function(model){            
+            
+            if(model !== clickedMesh){
+                
+                model.visible = false;
+                
+            }
+        });
+        
+        moveCameraToMesh(clickedMesh);
+
+
+        // Convert 3D point to 2D screen position
+        const screenPosition = point.clone().project(camera);
+        const x = (screenPosition.x * 0.5 + 0.5) * window.innerWidth;
+        const y = (screenPosition.y * -0.5 + 0.5) * window.innerHeight;
+
+        // Create and position the div
+        const infoDiv = document.createElement('div');
+        infoDiv.className = 'info-div';
+        infoDiv.style.left = `${x}px`;
+        infoDiv.style.top = `${y}px`;
+        infoDiv.innerHTML = `${clickedName} Clicked at<br>x: ${point.x.toFixed(2)}<br>y: ${point.y.toFixed(2)}<br>z: ${point.z.toFixed(2)}`;
+        document.body.appendChild(infoDiv);
+
+        const closeBtn = document.createElement('button');
+        closeBtn.style.position = 'absolute';
+        closeBtn.style.left = '100px';
+        closeBtn.style.top = '100px';
+        closeBtn.innerHTML = 'close';
+        document.body.appendChild(closeBtn);
+
+        $btn_init_camera.style.display = 'none';
+
+        
+
+        window.removeEventListener('click', windowClickHandler);
+        closeBtn.addEventListener('click', function(event){
+            infoDiv.remove();
+            event.currentTarget.remove();
+            gsap.to(camera.position, {
+                duration: 1.5,
+                x: old_camera_position.x,
+                y: old_camera_position.y,
+                z: old_camera_position.z,
+                ease: "power2.inOut"
+            });
+
+            $btn_init_camera.style.display = 'inline-block';
+
+            models.forEach(function(model){            
+                if(model !== clickedMesh){
+                    model.visible = true;
+                }
+            });
+            
+            setTimeout(function(){
+                window.addEventListener('click', windowClickHandler);
+            }, 1000);
+            
+        });
+    }
 }
 
 function draw(){                
@@ -156,6 +255,7 @@ function draw(){
 }
 
 function eventListener(){
+    window.addEventListener('click', windowClickHandler);
     window.addEventListener('resize', onWindowResize);   
     $btn_init_camera.addEventListener('click', init_camera);
 }
