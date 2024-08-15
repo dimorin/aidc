df7722e7aaa200175bc22db2fe4e7cb89344554a
dimorin <dimorinpk@gmail.com>
2024-08-16 00:52:55 +0900

    Add files via upload

--- src/js/aidc.js
+++ src/js/aidc.js
@@ -15,6 +15,9 @@ const $btn_init_camera = document.querySelector('.btn_init_camera');
 const clock = new THREE.Clock();
 
 
+const testColor = 'tomato';
+const floorColor = 'beige';
+
 if(WebGL.isWebGLAvailable()){    
     //console.log(THREE);
     init();    // 기본 환경 세팅 : scene, camera, renderer, orbitControls, light    
@@ -31,8 +34,8 @@ function init(){
 
     // SCENE
     scene = new THREE.Scene();
-    scene.background = new THREE.Color('#eeeeee');
-
+    scene.background = new THREE.Color('#eeeeee');    
+    scene.fog = new THREE.Fog(0x777777, 100, 800); // 땅의 경게를 부드럽게 처리하기 위한 효과
     // CAMERA
     camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.set(0,50,200);
@@ -71,7 +74,7 @@ function init(){
 
     // LIGHTS
     const dLight = new THREE.DirectionalLight('white', 0.8);
-    dLight.position.set(0, 30, 0);
+    dLight.position.set(-30, 80, 100);
     dLight.castShadow = true;
     dLight.shadow.mapSize.width = 2048;
     dLight.shadow.mapSize.height = 2048;
@@ -82,8 +85,12 @@ function init(){
     dLight.shadow.camera.bottom = -d;
     scene.add(dLight);
 
-    const aLight = new THREE.AmbientLight('white', 0.5);
-    scene.add(aLight);
+    const aLight = new THREE.AmbientLight('white', 0.6);
+    scene.add(aLight);    
+
+    // light helper
+    const dlightHelper = new THREE.DirectionalLightHelper(dLight,20);
+    scene.add(dlightHelper);    
 
     // axes
     const axesHelper = new THREE.AxesHelper(10);
@@ -95,6 +102,11 @@ function init(){
     folder1.add(camera.position, 'z', -100, 100);
 }
 
+function mathRandom(num=1){
+    var numValue = -Math.random() * num + Math.random() * num;
+    return numValue;
+}
+
 async function loadMap(){
     
     const gltfLoader = new GLTFLoader();
@@ -104,26 +116,84 @@ async function loadMap(){
         gltfLoader.loadAsync("./src/models/server3.glb"),
         gltfLoader.loadAsync("./src/models/server4.glb"),
         gltfLoader.loadAsync("./src/models/tower.glb"),
-        gltfLoader.loadAsync("./src/models/cloud.glb")
+        gltfLoader.loadAsync("./src/models/cloud.glb"),
+        gltfLoader.loadAsync("./src/models/sec.glb")
     ];      
     
     const [...load_model] = await Promise.all(promises);
     
     // model이 모두 로드 된 후 할 일    
     load_model.forEach(function(model, index){
+        //console.log(model.scene.children[0]);
         console.log(model.scene.children[0].name, model.scene.children[0].position);
+        if(model.scene.children[0].type === 'Group'){
+            model.scene.children[0].children.forEach(function(child){
+                console.log(child);
+                child.castShadow = true;
+                child.receiveShadow = true;
+            })
+        }else if(model.scene.children[0].type === 'Mesh'){
+            model.scene.children[0].castShadow = true;
+            model.scene.children[0].receiveShadow = true;
+        }
         if(model.scene.children[0].name === 'Cube021'){
-            model.scene.children[0].position.set(-10,5,100);
+            model.scene.children[0].position.set(-10,5,50);
+        }
+        if(model.scene.children[0].name === 'sec'){
+            model.scene.children[0].position.set(-60,15,-30);
         }
         
+        
         models.push(model.scene.children[0]);
-        scene.add(model.scene.children[0]);        
+        scene.add(model.scene.children[0]);     
+
+        console.log("--------");        
+        
     });
+    const test = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshStandardMaterial({color:testColor, side:THREE.DoubleSide}));
+    test.position.set(0,5,0);
+    scene.add(test);
+    test.castShadow = true;
+    test.receiveShadow = true;
     // floor
-    const floor = new THREE.Mesh(new THREE.CircleGeometry(100), new THREE.MeshPhongMaterial({color:'darkblue', side:THREE.DoubleSide}));
+    const floor = new THREE.Mesh(new THREE.CircleGeometry(100), new THREE.MeshStandardMaterial({color:floorColor, side:THREE.DoubleSide}));
     floor.name = 'floor';    
     floor.rotation.x = THREE.MathUtils.degToRad(90);
     scene.add(floor);
+    floor.receiveShadow = true;
+    // 바닥 그리드
+    const gridHelper = new THREE.GridHelper(200, 10, 'orange', 'orange');
+    scene.add(gridHelper);
+    // line
+    let lines = [];
+    function createLines(){
+        const lineMesh = new THREE.Mesh(new THREE.BoxGeometry(100, 0.2, 0.2), new THREE.MeshToonMaterial({color:'tomato'}));
+        lineMesh.position.x = mathRandom(100);
+        lineMesh.position.z = -1000;
+        lineMesh.position.y = Math.abs(mathRandom(100));
+        lineMesh.rotation.y = (90*Math.PI) / 180;
+        scene.add(lineMesh);
+        gsap.to(lineMesh.position, {
+            duration: 1.5,            
+            z: 50,
+            delay:mathRandom(5),
+            repeat:-1,
+            ease: "power2.inOut"
+        });
+    }
+    for(let i=0;i<100;i++){
+        createLines();
+    }
+    
+
+    let particleContainer = new THREE.Object3D();
+    for(var i=0; i<100; i++){
+        var particle = new THREE.Mesh(new THREE.CircleGeometry(1,10), new THREE.MeshToonMaterial({color:'orange'}));
+        particle.position.set(mathRandom(100), mathRandom(100), mathRandom(53));
+        particle.rotation.set(mathRandom(10), mathRandom(10), mathRandom(10));
+        particleContainer.add(particle);
+    }
+    scene.add(particleContainer);
 }
 
 // 카메라 이동 함수
@@ -172,7 +242,51 @@ function intersect(mouse) {
     raycaster.setFromCamera(mouse, camera);
     return raycaster.intersectObjects(scene.children);
 }
-function windowClickHandler(event){
+function canvasMoveHandler(event){
+    if(event.target.className === 'btn_init_camera'){
+        return;
+    }
+
+    // THREE RAYCASTER
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    const intersects = intersect(mouse);
+
+    if (intersects.length > 0) {
+        const intersect = intersects[0];
+        const point = intersect.point;
+        let clickedMesh = intersects[0].object;
+        let clickedName = intersects[0].object.name;
+        
+        if(intersect.object.parent.type === 'Group'){
+            clickedMesh = intersects[0].object.parent;
+            clickedName = intersects[0].object.parent.name;
+        }
+        
+        //console.log(clickedName);
+        models.forEach(function(model){            
+            
+            if(model !== clickedMesh){
+                
+                gsap.to(model.position, {
+                    duration: 1.5,            
+                    y:0,            
+                    ease: "power2.inOut"
+                });
+                
+            }else{
+                gsap.to(model.position, {
+                    duration: 1.5,            
+                    y:10,            
+                    ease: "power2.inOut"
+                });
+            }
+        });
+        
+    }
+}
+function canvasClickHandler(event){
     if(event.target.className === 'btn_init_camera'){
         return;
     }
@@ -234,7 +348,7 @@ function windowClickHandler(event){
 
         
 
-        window.removeEventListener('click', windowClickHandler);
+        $canvas.removeEventListener('click', canvasClickHandler);
         closeBtn.addEventListener('click', function(event){
             infoDiv.remove();
             event.currentTarget.remove();
@@ -255,7 +369,7 @@ function windowClickHandler(event){
             });
             
             setTimeout(function(){
-                window.addEventListener('click', windowClickHandler);
+                $canvas.addEventListener('click', canvasClickHandler);
             }, 1000);
             
         });
@@ -273,7 +387,8 @@ function draw(){
 }
 
 function eventListener(){
-    window.addEventListener('click', windowClickHandler);
+    $canvas.addEventListener('click', canvasClickHandler);
+    $canvas.addEventListener('mousemove', canvasMoveHandler);
     window.addEventListener('resize', onWindowResize);   
     $btn_init_camera.addEventListener('click', init_camera);
 }
